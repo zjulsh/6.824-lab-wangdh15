78fefbf42123c886002fa1b41f906f2d8ff74e11
wangdh15 <13020023780@163.com>
2022-05-19 23:40:29 +0800

    finish lab1 mapreduce and pass all test

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,13 +6,23 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
+
+type TaskType int
+
+const (
+	MapTask    TaskType = 0
+	ReduceTask TaskType = 1
+	WaitTask   TaskType = 2
+)
 
 type ExampleArgs struct {
 	X int
@@ -24,6 +34,25 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type AcTaskArgs struct {
+}
+
+type AcTaskReply struct {
+	Task_t  TaskType // task type
+	Task_id int      // task number
+	// input file names
+	Num_reducer   int
+	Map_file_name string
+	Map_task_num  int
+}
+
+type DoneTaskArgs struct {
+	Task_t  TaskType
+	Task_id int
+}
+
+type DoneTaskReply struct {
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,16 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"strconv"
+	"time"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,7 +30,6 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
@@ -33,6 +38,119 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// Your worker implementation here.
 
+	for {
+		args := AcTaskArgs{}
+		reply := AcTaskReply{}
+		ok := call("Coordinator.AcquireTask", &args, &reply)
+		if !ok {
+			log.Fatal("call rpc error! work exist!")
+		}
+		if reply.Task_t == MapTask {
+			filename := reply.Map_file_name
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Fatalf("cannot open %v", filename)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", filename)
+			}
+			file.Close()
+			kva := mapf(filename, string(content))
+			tmp_cont := make([][]KeyValue, reply.Num_reducer)
+			for i := 0; i < reply.Num_reducer; i++ {
+				tmp_cont[i] = []KeyValue{}
+			}
+			for _, kv := range kva {
+				idx := ihash(kv.Key) % reply.Num_reducer
+				tmp_cont[idx] = append(tmp_cont[idx], kv)
+			}
+			for i := 0; i < reply.Num_reducer; i++ {
+				out_file_name := fmt.Sprintf("mr-%d-%d", reply.Task_id, i)
+				ofile, err := ioutil.TempFile("", out_file_name)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				enc := json.NewEncoder(ofile)
+				for _, kv := range tmp_cont[i] {
+					err := enc.Encode(&kv)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+				ofile.Close()
+				// rename file atomic
+				os.Rename(ofile.Name(), out_file_name)
+			}
+			go func(task_id int) {
+				Done_args := DoneTaskArgs{}
+				Done_reply := DoneTaskReply{}
+				Done_args.Task_t = MapTask
+				Done_args.Task_id = task_id
+				ok := call("Coordinator.TaskDone", &Done_args, &Done_reply)
+				if !ok {
+					log.Fatal("call map done error!")
+				}
+			}(reply.Task_id)
+
+		} else if reply.Task_t == ReduceTask {
+			all_data := make(map[string][]string)
+			for i := 0; i < reply.Map_task_num; i++ {
+				filename := "mr-" + strconv.Itoa(i) + "-" + strconv.Itoa(reply.Task_id)
+				file, err := os.Open(filename)
+				if err != nil {
+					log.Fatalf("cannot open %v", filename)
+				}
+				dec := json.NewDecoder(file)
+				for {
+					var kv KeyValue
+					if err := dec.Decode(&kv); err != nil {
+						break
+					}
+					all_data[kv.Key] = append(all_data[kv.Key], kv.Value)
+				}
+				file.Close()
+			}
+
+			reduce_out := []KeyValue{}
+			for k, v := range all_data {
+				tmp_data := KeyValue{k, reducef(k, v)}
+				reduce_out = append(reduce_out, tmp_data)
+			}
+
+			out_put_file := "mr-out-" + strconv.Itoa(reply.Task_id)
+			ofile, err := ioutil.TempFile("", out_put_file)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, data := range reduce_out {
+				fmt.Fprintf(ofile, "%v %v\n", data.Key, data.Value)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			ofile.Close()
+			os.Rename(ofile.Name(), out_put_file)
+
+			go func(task_id int) {
+				args := DoneTaskArgs{}
+				reply := DoneTaskReply{}
+				args.Task_id = task_id
+				args.Task_t = ReduceTask
+				ok := call("Coordinator.TaskDone", &args, &reply)
+				if !ok {
+					log.Fatal("reduce call taskdone error!")
+				}
+			}(reply.Task_id)
+		} else if reply.Task_t == WaitTask {
+			time.Sleep(1 * time.Second)
+		} else {
+			fmt.Println("unrecognized task type!")
+			os.Exit(1)
+		}
+	}
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
